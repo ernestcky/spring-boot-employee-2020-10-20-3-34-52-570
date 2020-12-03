f5c5f8e7a8b1e56525c975555e381499ab7221c7
Ernest Cheung <ernest.cheung@oocl.com>
2020-12-04 01:21:35 +0800

    [CompanyIntegrationTest][Ernest] Feat: return all company when get all

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @AfterEach
+    void tearDown() {
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    public void should_return_all_companies_when_get_all_given_companies() throws Exception {
+        //given
+        Company company = new Company("Company1", 100);
+        companyRepository.insert(company);
+
+        //when
+        mockMvc.perform(get("/companies"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$[0].companyId").isString())
+                        .andExpect(jsonPath("$[0].companyName").value("Company1"))
+                        .andExpect(jsonPath("$[0].employeesNumber").value(100));
+    }
+
+}
