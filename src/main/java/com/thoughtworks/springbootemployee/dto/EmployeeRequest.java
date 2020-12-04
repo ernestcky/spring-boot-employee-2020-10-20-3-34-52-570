1f06f2a762558ce2047fa752872778f0229b60bd
Ernest Cheung <ernest.cheung@oocl.com>
2020-12-04 14:50:12 +0800

    [EmployeeRequest][Ernest] Added employee request class

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeRequest.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class EmployeeRequest {
+    private final String name;
+    private final Integer age;
+    private final String gender;
+    private final Integer salary;
+
+    public EmployeeRequest(String name, Integer age, String gender, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+}
